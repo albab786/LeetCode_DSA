fc3be5e9de67edef3649406a49b1e31a112aac35
Albab <116362742+albab786@users.noreply.github.com>
2025-08-14 01:16:10 +0530

    Time: 2 ms (96.54%) | Memory: 42.2 MB (12.52%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+    for (char c : s.toCharArray()) {
+        if (c == '(' || c == '{' || c == '[') {
+            stack.push(c);
+        } else {
+            if (stack.isEmpty()) return false; // no matching opening
+            char top = stack.peek();
+            if ((c == ')' && top == '(') ||
+                (c == ']' && top == '[') ||
+                (c == '}' && top == '{')) {
+                stack.pop();
+            } else {
+                return false; // mismatched pair
+            }
+        }
+    }
+    return stack.isEmpty();
+    }
+}
